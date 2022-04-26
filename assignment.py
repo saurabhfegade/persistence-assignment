732737cf35e4fdc3e8cbfb8ca74f2636774e38e3
saurabhfegade <saurabh.fegade1998@gmail.com>
2022-04-27 04:06:07 +0530

    another solution

--- assignment.py
+++ assignment.py
@@ -1,100 +1,39 @@
-import ast
-from operator import itemgetter
-import re
-from time import clock_getres
-# from memory_profiler import profile
 from argparse import ArgumentParser
-from tempfile import TemporaryDirectory, mktemp
-import os, sys
-import heapq
-from contextlib import contextmanager
+import sys
+from functools import partial
+from heapq import merge
+from tempfile import TemporaryFile
 
-def key_funct(line):
-    list_line = ast.literal_eval(line)
-    print("List Line", list_line)
-    return [x[1] for x in list_line] 
-
-
-def large_sort(input_file, output_file, reverse: bool=False):
-
-    with TemporaryDirectory() as tmp_dir:
-        lines2 = []
-        for lines in _read_parts(input_file):
-            dict1 = {}
-            for l in lines:
-                ze_tokens = l.split(' ')
-                dict1[ze_tokens[0]] = int(ze_tokens[1])
-            # lines1 = sorted(dict1, key=dict1.get, reverse=True)
-            # print("LINES", lines1)
-            lines1 = sorted(dict1.items(), key=lambda x: x[1], reverse=True)
-            print("lines1", lines1)
-            lines2.append(lines1)
-            print("lines2", lines2)
-            _write_part(lines1, tmp_dir)
-
-        with _open_tmp_files(tmp_dir) as tmp_files:
-            print("tmp1: ", tmp_files[0].read(), "tmp2:", tmp_files[1].readlines())
-            print("Heap", list(heapq.merge(*tmp_files, key=key_funct, reverse=True)))
-            for row in heapq.merge(*tmp_files, key=key_funct, reverse=True):
-                print('Heapq \n', row)
-                # output_file.write(row)
-                # print(output_file)
-        
-    # return lines1
-    # with _open_tmp_files(tmp_dir) as tmp_files:
-    #     for row in heapq.merge(*tmp_files, key=key, reverse=reverse):
-    #         output_file.write(row)
-
-
-def _read_parts(input_file):
-    lines = input_file.readlines(40)
-    print(2, lines)
-    while lines:
-        yield lines
-        lines = input_file.readlines(40)
-
-def _write_part(lines, tmp_dir):
-    print("call")
-    tmp_filename = mktemp(dir=tmp_dir)
-    with open(tmp_filename, "w") as tmp_file:
-        # lines1 = sorted(lines, key=lines.get, reverse=True)
-        # lines1 = dict(sorted(lines.items(), key=lambda x: x[1], reverse=True))
-        # print("A", lines1)
-        tmp_file.writelines(str(lines))
-    return tmp_filename
-
-@contextmanager
-def _open_tmp_files(tmp_dir):
-    filenames = os.listdir(tmp_dir)
-    files = [open(os.path.join(tmp_dir, filename), "r") for filename in filenames]
+# sorting criteria
+def key_func(line):
     try:
-        yield files
-    finally:
-        for file in files:
-            file.close()
+        return int(line.split(" ")[1]) 
+    except (IndexError, ValueError):
+        return None 
+
 
 def _parse_args():
     parser = ArgumentParser()
-    parser.add_argument("file", nargs="?", help="入力ファイル")
-    parser.add_argument("out_file", nargs="?", help="入力ファイル")
+    parser.add_argument("count", default=2)
     return parser.parse_args()
 
-# @profile(precision=4)
 def main():
     args = _parse_args()
-    print("args", args)
-    file = open(args.file, "r")
-    large_sort(file, sys.stdout)
-    # print("Newlines", newLines)
-    # dict1 = {}
-    # ze_file = open("input.txt", 'r')
-    # ze_lines = ze_file.readlines(150)
-    # print("ze",ze_lines)
-    # for l in ze_lines:
-    #     ze_tokens = l.split(' ')
-    #     dict1[ze_tokens[0]] = int(ze_tokens[1])
-    # print(dict1)
-    # print(sorted(dict1, key=dict1.get, reverse=True)[0:3])
-    # ze_file.close()
+    sorted_files = []
+    nbytes = 1 << 20 
+    for lines in iter(partial(sys.stdin.readlines, nbytes), []):
+        lines.sort(key=key_func, reverse=True) 
+        f = TemporaryFile("w+")
+        f.writelines(lines)
+        f.seek(0)
+        sorted_files.append(f)
+
+    merged_list =  list(merge(*sorted_files, key=key_func, reverse=True))
+
+    sys.stdout.writelines([f'{x.split(" ")[0]}\n' for x in merged_list[0:int(args.count)]])
+
+    # clean up
+    for f in sorted_files:
+        f.close()
 
 main()
