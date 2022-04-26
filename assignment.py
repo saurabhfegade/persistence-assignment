6f559ff7f2bd031f956b0340eeb7392187e55d01
saurabhfegade <saurabh.fegade1998@gmail.com>
2022-04-27 04:53:10 +0530

    add comments

--- assignment.py
+++ assignment.py
@@ -1,3 +1,9 @@
+"""
+Usage: 
+From the command line - python assignment.py 5 < input.txt > output.txt
+Here 5 is the count to return 5 largest numbers
+"""
+
 from argparse import ArgumentParser
 import sys
 from functools import partial
@@ -7,11 +13,11 @@ from tempfile import TemporaryFile
 # sorting criteria
 def key_func(line):
     try:
-        return int(line.split(" ")[1]) 
+        return int(line.split(" ")[1]) # e.g. for (1426828011 9) it will select second element to sort with
     except (IndexError, ValueError):
         return None 
 
-
+# take count by parsing arguments
 def _parse_args():
     parser = ArgumentParser()
     parser.add_argument("count", default=2)
@@ -20,7 +26,7 @@ def _parse_args():
 def main():
     args = _parse_args()
     sorted_files = []
-    nbytes = 1 << 20 
+    nbytes = 1 << 20            # 1048576 bytes at a time to be used in memory
     for lines in iter(partial(sys.stdin.readlines, nbytes), []):
         lines.sort(key=key_func, reverse=True) 
         f = TemporaryFile("w+")
@@ -28,9 +34,9 @@ def main():
         f.seek(0)
         sorted_files.append(f)
 
-    merged_list =  list(merge(*sorted_files, key=key_func, reverse=True))
+    merged_list =  list(merge(*sorted_files, key=key_func, reverse=True))   # merge the sorted temporary files based on key function
 
-    sys.stdout.writelines([f'{x.split(" ")[0]}\n' for x in merged_list[0:int(args.count)]])
+    sys.stdout.writelines([f'{x.split(" ")[0]}\n' for x in merged_list[0:int(args.count)]])     # write into stdout the "COUNT" largest elements  
 
     # clean up
     for f in sorted_files:
